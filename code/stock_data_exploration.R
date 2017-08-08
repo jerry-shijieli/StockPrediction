e0e9726e38bb00ea4496b35fbbacc436026d6757
sli41 <sli41@ncsu.edu>
2017-08-07 23:26:08 -0400

    update project code and results

--- code/stock_data_exploration.R
+++ code/stock_data_exploration.R
@@ -7,7 +7,8 @@ library(readr)
 library(ggplot2)
 
 raw_data <- read_csv('../data/IBEX35.csv', col_names=TRUE)
-tsprice <- xts(x=raw_data$Close, order.by = as.Date(raw_data$Date,"%m/%d/%y"), frequency = 255)
+tsprice <- xts(x=raw_data$Price, order.by = as.Date(raw_data$Date,"%m/%d/%y"), frequency = 255)
 summary(tsprice)
 
-plot(tsprice, type='l', main='Close price of stock', xlab='Date', ylab='Close Price')
+plot(tsprice, type='l', main='Price of stock', xlab='Date', ylab='Close Price')
+
