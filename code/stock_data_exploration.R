328bf15e9856d923e0c3c5e930b6ac0de097479d
sli41 <sli41@ncsu.edu>
2017-08-08 13:36:23 -0400

    update project code and results

--- code/stock_data_exploration.R
+++ code/stock_data_exploration.R
@@ -10,5 +10,15 @@ raw_data <- read_csv('../data/IBEX35.csv', col_names=TRUE)
 tsprice <- xts(x=raw_data$Price, order.by = as.Date(raw_data$Date,"%m/%d/%y"), frequency = 255)
 summary(tsprice)
 
-plot(tsprice, type='l', main='Price of stock', xlab='Date', ylab='Close Price')
+plot(tsprice, type='l', main='Price of stock', xlab='Date', ylab='Price')
 
+tsprice_train <- tsprice[index(stock_log_return)<as.Date("2015-01-01")]
+tsprice_test <- tsprice[index(stock_log_return)>=as.Date("2015-01-01")]
+
+fit_model <- auto.arima(tsprice_train)
+
+arima_model <- arima(tsprice_train, order=c(0,1,0))
+
+test_pred <- forecast.Arima(arima_model, h=length(tsprice_test))
+
+plot.forecast(test_pred)
